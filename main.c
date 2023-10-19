31f1dd5226fcdcf2972104d6b482a27f6a0dfe5c
gwa8520 <144990431+gwa8520@users.noreply.github.com>
2023-10-19 15:37:48 +0900

    23.10.19 3

--- main.c
+++ main.c
@@ -3,16 +3,19 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
+int inc(int counter)  //prototyping
 int main(int argc, char *argv[]) {
 	
-	int i;
-	//int temp=1; 
-	
-	for(i=0;i<5;i++){    //temp가 1의 값만 갖는다. temp는 블록이 끝나면 소멸. i가 다시 2로 들어가면 temp가 새로 생성.    
-		int temp=1;  //static int temp=1;
-		printf("temp is %d\n",temp);
-		temp++;
-	}  
+	int i=10;
+	printf("함수 호출 전 i=%d",i);  //i=10  
+	inc(i);
+	printf("함수 호출 후 i=%d",i)  //i=10, counter함수는 inc함수가 끝나면 소멸하기 때문이다.  
 	
 	return 0;
 }
+
+int inc(int counter){
+	
+	counter++;
+	return counter;
+} 
