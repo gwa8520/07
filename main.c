22ea4eae5866912bfd9998896ad6ecb13322d053
gwa8520 <144990431+gwa8520@users.noreply.github.com>
2023-10-19 15:57:24 +0900

    23.10.19 4

--- main.c
+++ main.c
@@ -2,20 +2,19 @@
 #include <stdlib.h>
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
+ void f(void);
+ 
+ int i; 
 
-int inc(int counter)  //prototyping
 int main(int argc, char *argv[]) {
-	
-	int i=10;
-	printf("함수 호출 전 i=%d",i);  //i=10  
-	inc(i);
-	printf("함수 호출 후 i=%d",i)  //i=10, counter함수는 inc함수가 끝나면 소멸하기 때문이다.  
-	
-	return 0;
-}
-
-int inc(int counter){
-	
-	counter++;
-	return counter;
+	for(i=0;i<5;i++){  //i의 값이 5보다 더 크기에 한번만 실행 후 종료된다.  
+		f();  
+	}
+return 0;
 } 
+
+void f(void){
+	for(i=0;i<10;i++){
+		printf("#");   //여기서 전역변수 i의 값이 10으로고정  
+	}
+}
